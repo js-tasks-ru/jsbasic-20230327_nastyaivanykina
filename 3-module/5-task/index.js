9025c5733f7dcb11ae568c243577cd9426a770aa
n.ivanykina <nastyaivanykina@gmail.com>
2023-05-08 18:46:07 +0300

    tasks 4-5 of 3 module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,6 @@
 function getMinMax(str) {
-  // ваш код...
+  const nums = str.split(" ").map(parseFloat).filter(isFinite);
+  const min = Math.min(...nums);
+  const max = Math.max(...nums);
+  return { min, max };
 }
