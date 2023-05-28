2e00651b42e63a1098bc1ed3ff620568edcc7aa8
n.ivanykina <nastyaivanykina@gmail.com>
2023-05-28 23:51:49 +0300

    tasks of 4 module

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rows = table.rows.length;
+  for (let i = 0; i < rows; i++) {
+    let cell = table.rows[i].cells[i];
+    cell.style.backgroundColor = 'red';
+  }
 }
